3ed920b3eaa0d8c18d77aa6c120ba5336c67fe4b
hubeng <kosinad@gasos-ro.cz>
2023-03-16 15:27:07 +0100

    dodelal jsem to jako kral

--- app/src/main/java/com/example/rocnikovaprace/temac1.kt
+++ app/src/main/java/com/example/rocnikovaprace/temac1.kt
@@ -17,25 +17,63 @@ class temac1 : AppCompatActivity() {
 
         val btnTemac1 = findViewById<Button>(R.id.zpet)
         btnTemac1.setOnClickListener {
-            val intent = Intent(this,MainActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
-        val btn = findViewById<Button>(R.id.btnpotvrdit)
+        val plusmore = findViewById<Button>(R.id.plus)
+        val minusmore = findViewById<Button>(R.id.minus)
+        val delenomore = findViewById<Button>(R.id.deleno)
+        val kratmore = findViewById<Button>(R.id.krat)
         val cislo1 = findViewById<EditText>(R.id.cislo1)
         val cislo2 = findViewById<EditText>(R.id.cislo2)
         val vysledek = findViewById<TextView>(R.id.vysledek)
 
-        btn.setOnClickListener{
-            val soucet = (Integer.parseInt(cislo1.text.toString())+Integer.parseInt(cislo2.text.toString()))
-            vysledek.setText(soucet)
-
-
-
+        plusmore.setOnClickListener {
+            validace(cislo1.text.toString(), cislo2.text.toString(), '+')
         }
+
+        minusmore.setOnClickListener {
+            validace(cislo1.text.toString(), cislo2.text.toString(), '-')
+        }
+        delenomore.setOnClickListener {
+            validace(cislo1.text.toString(), cislo2.text.toString(), '/')
+        }
+        kratmore.setOnClickListener {
+            validace(cislo1.text.toString(), cislo2.text.toString(), '*')
+        }
+
 
 
 
     }
 
-}
+    fun validace(prvni: String, druhy: String, operator: Char) {
+        val cislo1 = findViewById<EditText>(R.id.cislo1)
+        val cislo2 = findViewById<EditText>(R.id.cislo2)
+        val vysledek = findViewById<TextView>(R.id.vysledek)
+        if (prvni.isEmpty()) {
+            cislo1.error = "zadejte číslo!"
+        }
+        if (druhy.isEmpty()) {
+            cislo2.error = "zadejte číslo!"
+        }
+        if (prvni.isNotEmpty() && druhy.isNotEmpty()) {
+            calculation(prvni.toDouble(), druhy.toDouble(), operator)
+        }
+    }
+
+    fun calculation(prvni: Double, druhy: Double, operator: Char) {
+        val vysledek = findViewById<TextView>(R.id.vysledek)
+        var result = 0.0
+        when (operator) {
+            '+' -> result = prvni + druhy
+            '-' -> result = prvni - druhy
+            '*' -> result = prvni * druhy
+            '/' -> result = prvni / druhy
+        }
+        vysledek.text = result.toString()
+    }
+    }
+
+
